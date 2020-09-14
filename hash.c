27011bc614a0cc5214ed659a665eba82c4896065
AnyVictor <53903774+AnyVictor@users.noreply.github.com>
2020-09-14 16:11:09 -0300

    Update hash.c

--- hash.c
+++ hash.c
@@ -8,20 +8,33 @@
 #include <linux/slab.h>
 #include <crypto/hash.h>
 
+#define SIZE_SHA1 20 //Em bytes
+
 void hash(char* string,int size_of_message){
-	struct shash_desc *desc; //serve para configurar o hashing
-	struct crypto_shash *alg; //shash
-	char hashval; //armazena a hash
+	struct shash_desc *desc; //Serve para configurar o hashing type
+	struct crypto_shash *tfm; //Variavel que vai receber cipher handle
+	char *hashval; //Armazena a hash da nossa sting
 	int final;
 	string[0]='7';
 
-	alg = crypto_alloc_shash("sha1", 0, 0); //Allocate a cipher handle for an shash
+	tfm = crypto_alloc_shash("sha1", 0, 0); //Allocate a cipher handle for an shash *(const char * alg_name, u32 type, u32 mask)* 
 	desc = vmalloc(sizeof(struct shash_desc));
-	hashval = vmalloc(SHA1_SIZE_BYTES + 1);
+    if(!desc)goto hash_skip;
 
-	crypto_shash_init(desc);
-	crypto_shash_update(desc, string, size_of_message);
-	final = crypto_shash_final(desc, hashval);
+    desc->tfm = tfm; //Iniciando variavel
+    desc->flags = 0x0; //Iniciando variavel
 
-	return final;
+	hashval = vmalloc(SIZE_SHA1 + 1);
+    if(!hashval)goto hash_skip;
+
+	crypto_shash_init(desc); //Configures hashing engine according to the supplied hash_desc
+	crypto_shash_update(desc, string, size_of_message); //Performs the actual hashing method on sting
+	final = crypto_shash_final(desc, hashval); //Sends hash to character array
+
+hash_skip:
+    if(tfm) crypto_free_shash(tfm);
+    if(desc) vfree(desc);
+    if(hashval) vfree(hashval);
+    return final;
 }
+
